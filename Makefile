87c13f1bc8c5a79081fb427701297e1ad60bc93b
James Deery <james.deery@jadex.org>
2015-06-19 22:39:44 +0100

    Split tree code into a separate file

--- Makefile
+++ Makefile
@@ -1,15 +1,17 @@
-SRCS := mktree.c
 CFLAGS += -Wall -std=c11
 
 all: tree.svg
 
-clean:
-	rm -f mktree tree.svg
+tree.o: tree.c tree.h
+mktree.o: mktree.c tree.h
 
-mktree: $(SRCS)
+mktree: tree.o mktree.o
 
 tree.svg: mktree
 	./mktree > tree.svg
+
+clean:
+	rm -f *.o mktree tree.svg
 
 .PHONY: all clean
 
---
+++ tree.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2015 Ben Mather, James Deery
+ * Released under the MIT license
+ * See COPYING for details.
+ */
+
+#define MAX_CHILDREN 5
+
+typedef struct Branch Branch;
+struct Branch {
+    float length;
+    float thickness;
+    float angle;
+
+    struct Branch *children[MAX_CHILDREN];
+    int num_children;
+};
+
+Branch *make_tree();
+void render_tree(Branch *tree);
+
